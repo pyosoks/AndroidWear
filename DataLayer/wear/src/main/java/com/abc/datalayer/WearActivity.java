d2c465a15dffc761b757469cc622eb43975d905a
pyosoks <pyosoks@gmail.com>
2014-12-01 20:13:29 +0900

    HandHeld Device 로부터 text 를 전달 받으면 출력 할 Wearable Device의 UI 구성

--- DataLayer/wear/src/main/java/com/abc/datalayer/WearActivity.java
+++ DataLayer/wear/src/main/java/com/abc/datalayer/WearActivity.java
@@ -2,12 +2,14 @@ package com.abc.datalayer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.wearable.view.WatchViewStub;
-import android.widget.TextView;
+import android.widget.EditText;
 
 public class WearActivity extends Activity {
 
-    private TextView mTextView;
+    EditText console;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +19,27 @@ public class WearActivity extends Activity {
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
+                // EditText 의 참조값 얻어오기
+                console = (EditText)findViewById(R.id.console);
             }
         });
+    }
+
+    // 로그 찍는 메소드
+    public void printLog(String msg){
+        // 출력할 문자열을 메세지 객체에 담는다.
+        Message m = new Message();
+        m.obj = msg;
+    }
+
+    // 핸들러 객체 정의하기
+    Handler handler = new Handler(Message msg){
+        @Override
+        public void handleMessage(Message msg) {
+            // 전달된 문자열을 얻어온다.
+            String str = (String)msg.obj;
+            // EditText 에 개행기호와 함께 출력하기
+            console.append(str + "\n");
+        }
     }
 }
