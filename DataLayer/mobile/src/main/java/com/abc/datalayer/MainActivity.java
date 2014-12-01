b63525f8aff2ba7f92702f626c7d87cfb1c81268
pyosoks <pyosoks@gmail.com>
2014-12-01 17:49:01 +0900

    HandHeld 에서 텍스트 입력 받아 전송 할 코드 추가

--- DataLayer/mobile/src/main/java/com/abc/datalayer/MainActivity.java
+++ DataLayer/mobile/src/main/java/com/abc/datalayer/MainActivity.java
@@ -2,38 +2,50 @@ package com.abc.datalayer;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends Activity {
+    // 필요한 멤버필드 정의하기
+    EditText console, inputText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // EditText 의 참조값을 얻어와서 멤버필드에 저장하기
+        console = (EditText)findViewById(R.id.console);
+        inputText = (EditText)findViewById(R.id.inputText);
+    }
+    // 전송 버튼을 눌렀을 때 호출되는 메소드
+    public void send(View v){
+        // 입력한 문자열을 읽어온다.
+        String msg = inputText.getText().toString();
+
+        if(msg.equals(""))
+            return;
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    // 로그 찍는 메소드
+    public void printLog(String msg){
+        // 출력할 문자열을 메세지 객체에 담는다.
+        Message m = new Message();
+        m.obj = msg;
+        // 핸들러 메세지를 보낸다.
+        handler.sendMessage(m);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    // 핸들러 객체 정의하기
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            // 전달된 문자열을 얻어온다.
+            String str = (String)msg.obj;
+            // EditText 에 개행기호와 함께 출력하기
+            console.append(str + "\n");
         }
-
-        return super.onOptionsItemSelected(item);
-    }
+    };
 }
