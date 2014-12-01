0f0ed23a4a0f2c2da5aa4c9945e1c7c66cab24c9
pyosoks <pyosoks@gmail.com>
2014-12-01 20:31:18 +0900

    메세지 도착 리스너를 등록

--- DataLayer/wear/src/main/java/com/abc/datalayer/WearActivity.java
+++ DataLayer/wear/src/main/java/com/abc/datalayer/WearActivity.java
@@ -51,6 +51,8 @@ public class WearActivity extends Activity
     @Override
     protected void onStop() {
         gClient.disconnect();
+        // 등록된 listener 를 제가한다.
+        Wearable.MessageApi.removeListener(gClient, this);
         super.onStop();
     }
 
@@ -76,6 +78,8 @@ public class WearActivity extends Activity
     @Override
     public void onConnected(Bundle bundle) {
         printLog("onConnected()");
+        // 메세지 도착 리스너를 등록한다.
+        Wearable.MessageApi.addListener(gClient, this);
     }
 
     @Override
@@ -88,8 +92,12 @@ public class WearActivity extends Activity
         printLog("onConnectionFailed()");
     }
 
+    // Message 가 도착했을때 호출되는 메소드
     @Override
     public void onMessageReceived(MessageEvent messageEvent) {
-
+        // 전달된 path(메세지, 문자열)를 읽어온다.
+        String msg = messageEvent.getPath();
+        // 전달된 메세지를 EditText에 출력하기
+        printLog(msg);
     }
 }
