348e711d8d67dcdd38caecf7e97db8fdc6d66afa
pyosoks <pyosoks@gmail.com>
2014-12-01 20:21:49 +0900

    HandHeld Device 와 동일하게 GoogleApiClient 를 추가

--- DataLayer/wear/src/main/java/com/abc/datalayer/WearActivity.java
+++ DataLayer/wear/src/main/java/com/abc/datalayer/WearActivity.java
@@ -7,9 +7,17 @@ import android.os.Message;
 import android.support.wearable.view.WatchViewStub;
 import android.widget.EditText;
 
-public class WearActivity extends Activity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.Wearable;
+
+public class WearActivity extends Activity
+            implements GoogleApiClient.ConnectionCallbacks,
+                        GoogleApiClient.OnConnectionFailedListener{
 
     EditText console;
+    // GoogleApiClient 객체
+    GoogleApiClient gClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,24 @@ public class WearActivity extends Activity {
                 console = (EditText)findViewById(R.id.console);
             }
         });
+        // GoogleApiClient 객체의 참조값을 얻어와서 멤버필드에 저장하기
+        gClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        gClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        gClient.disconnect();
+        super.onStop();
     }
 
     // 로그 찍는 메소드
@@ -33,7 +59,7 @@ public class WearActivity extends Activity {
     }
 
     // 핸들러 객체 정의하기
-    Handler handler = new Handler(Message msg){
+    Handler handler = new Handler(){
         @Override
         public void handleMessage(Message msg) {
             // 전달된 문자열을 얻어온다.
@@ -41,5 +67,21 @@ public class WearActivity extends Activity {
             // EditText 에 개행기호와 함께 출력하기
             console.append(str + "\n");
         }
+    };
+
+    // GoogleApiClient 에 연결 되었을대 호출되는 메소드
+    @Override
+    public void onConnected(Bundle bundle) {
+        printLog("onConnected()");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        printLog("onConnectionSuspended()");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        printLog("onConnectionFailed()");
     }
 }
